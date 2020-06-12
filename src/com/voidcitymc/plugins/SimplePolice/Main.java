027a9d2365502a863a654b120732753b7c201e16
BuildTools <unconfigured@null.spigotmc.org>
2020-06-11 18:55:03 -0700

    removed enclosing simple police folder

---
+++ src/com/voidcitymc/plugins/SimplePolice/Main.java
@@ -0,0 +1,76 @@
+package com.voidcitymc.plugins.SimplePolice;
+
+import java.io.File;
+import java.util.HashMap;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.inventory.InventoryView;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.voidcitymc.plugins.SimplePolice.Main;
+
+public class Main extends JavaPlugin {
+private static Main instance;
+
+File DataFile;
+FileConfiguration Data;
+
+static HashMap<String, String> lastArrest = new HashMap<String, String>();
+
+public void createData() {
+    DataFile = new File(getDataFolder(), "data.yml");
+    if (!DataFile.exists()) {
+        DataFile.getParentFile().mkdirs();
+        saveResource("data.yml", false);
+     }
+    Data = YamlConfiguration.loadConfiguration(DataFile);
+}
+
+public static Main getInstance() {
+	return instance;
+}
+
+public void SaveDataFile() {
+	try {
+		Data.save(DataFile);
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+}
+
+//enabled
+@Override
+public void onEnable() {
+	//update checker
+	new UpdateChecker(this).checkForUpdate();
+	//metrics
+	@SuppressWarnings("unused")
+	Metrics metrics = new Metrics(this, 6814);
+	//create config
+	this.getConfig().options().copyDefaults(true);
+	saveConfig();
+	//create datafile;
+	createData();
+	//add mising items to config
+//	worker.AddMissingItemsToConfig();
+	//
+	getServer().getPluginManager().registerEvents(new GUI(), this);
+	getServer().getPluginManager().registerEvents(new PoliceListener(), this);
+	instance = this;
+	this.getCommand("police").setExecutor(new Police());
+	this.getCommand("911").setExecutor(new NineOneOne());
+	System.out.println("ramdon_person's Police Plugin Has Been Enabled!");
+}
+
+
+//disabled
+@Override
+public void onDisable() {
+	System.out.println("Thanks for using ramdon_person's police plugin!");
+	System.out.println("-- Saving Data --");
+	this.saveConfig();
+	this.SaveDataFile();
+	System.out.println("-- All data saved! --");
+}
+}
---
+++ src/com/voidcitymc/plugins/SimplePolice/NineOneOne.java
@@ -0,0 +1,74 @@
+package com.voidcitymc.plugins.SimplePolice;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+
+
+public class NineOneOne implements CommandExecutor {
+
+// This method is called, when somebody uses our command
+@Override
+public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+if (sender instanceof Player) {
+Player player = (Player) sender;
+
+
+ArrayList<Player> playerList = new ArrayList<Player>();
+
+ArrayList<String> policeList = worker.ListPolice();
+int i = 0;
+
+
+//online police list
+while (i < policeList.size()) {
+	if (Bukkit.getPlayer(UUID.fromString(policeList.get(i))) != null) {
+		playerList.add(Bukkit.getPlayer(UUID.fromString(policeList.get(i))));
+	}
+	i++;
+}
+
+//send police message
+
+i = 1;
+String message = "";
+if (args.length > 0) {
+	message = "- " + args[0];
+}
+
+while (i < args.length) {
+	message = message + " " + args[i];
+	i++;
+}
+
+
+i = 0;
+
+while (i < playerList.size()) {
+	playerList.get(i).sendMessage(ChatColor.DARK_AQUA + "[911] " + ChatColor.WHITE + sender.getName() + " needs help! "+message);
+	i++;
+}
+
+
+
+if (playerList.size() == 0) {
+	player.sendMessage(ChatColor.DARK_AQUA + "There are no police online, you are on your own!");
+} else {
+	player.sendMessage(ChatColor.DARK_AQUA + "The police have gotten your message and will come asap!");
+}
+
+
+
+}
+return true;
+}
+
+
+}
