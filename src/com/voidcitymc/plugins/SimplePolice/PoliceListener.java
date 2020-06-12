9fd9df935c220b3cb7b221e7a1464b6a788c21c2
BuildTools <unconfigured@null.spigotmc.org>
2020-06-11 18:55:03 -0700

    removed enclosing simple police folder

---
+++ src/com/voidcitymc/plugins/SimplePolice/PoliceListener.java
@@ -0,0 +1,44 @@
+package com.voidcitymc.plugins.SimplePolice;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+//import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+
+public class PoliceListener implements Listener {
+	
+    @EventHandler
+	public void onDamage(EntityDamageByEntityEvent event) {
+    	worker work = new worker();
+    	if (event.getEntity() instanceof Player && event.getDamager() instanceof Player) {
+    		//check if the player who punched someone is a police and has the police bitan
+    		Entity damagerE = (Entity) event.getDamager();
+    		Player damagerP = (Player) damagerE;
+    		Entity damageeE = (Entity) event.getEntity();
+    		Player damageeP = (Player) damageeE;
+    		//damager is the police
+    		//damagee is the criminal
+    		if (work.alreadyPolice(damagerP.getUniqueId().toString()) && worker.TestForItem(damagerP, Material.BLAZE_ROD, "Police")) /*put stuff here too) */ {
+    			System.out.println("A player has been arrested");
+    			damageeP.sendMessage("You have been arrested");
+    			Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "essentials:jail "+damageeP.getName()+" jail1");
+    			worker.PayPoliceOnArrest(damagerP);
+    			worker.TakeMoneyOnArrest(damageeP);
+    			Main.lastArrest.put(damagerP.getName(), damageeP.getName());
+    			GUI gui = new GUI();
+    			gui.openInventory(damagerP);
+    		}
+    	
+    		
+    		
+    		
+    		
+    	}
+	}
+	
+	
+}
