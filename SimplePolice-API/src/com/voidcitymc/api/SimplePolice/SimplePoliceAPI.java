8d9507d9cbbdacd584da4ff1599006a1e1a2eb92
ramdon-person <fierceeo@gmail.com>
2020-07-11 18:05:52 -0700

    fix java doc

--- SimplePolice-API/src/com/voidcitymc/api/SimplePolice/SimplePoliceAPI.java
+++ SimplePolice-API/src/com/voidcitymc/api/SimplePolice/SimplePoliceAPI.java
@@ -6,75 +6,74 @@ import org.bukkit.entity.Player;
 
 public interface SimplePoliceAPI {
 	
-	
-ArrayList<String> onlinePoliceList();
 	/**
 	 * List of all the online police
 	 * 
 	 * @return ArrayList<String> An ArrayList of all the online police user names.
 	 */
+ArrayList<String> onlinePoliceList();
 
-	
+
+/**
+ * Adds a police
+ * 
+ * @param uuid The uuid (in string format) of the player to add
+ */
+
 void addPolice (String uuid);
-	/**
-	 * Adds a police
-	 * 
-	 * @param uuid The uuid (in string format) of the player to add
-	 */
 
+/**
+ * Checks if a player is a police
+ * 
+ * @param uuid The uuid (in string format) of the player to check
+ * 
+ * @return boolean Returns if the player is a police
+ */
 
 boolean isPolice (String uuid);
-	/**
-	 * Checks if a player is a police
-	 * 
-	 * @param uuid The uuid (in string format) of the player to check
-	 * 
-	 * @return boolean Returns if the player is a police
-	 */
-
-
+/*
+ * Remove a police from the list
+ * 
+ * @param uuid The uuid (in string format) of the player to remove
+ */
 void removePolice(String uuid);
-        /*
-         * Remove a police from the list
-         * 
-         * @param uuid The uuid (in string format) of the player to remove
-         */
 
+/*
+ * Police teleport
+ * 
+ * @param player The player to teleport
+ * @param farthestTpDistance The farthest distance to teleport a player from another player
+ * 
+ * @return Location The random location given to teleport to
+ */
 Location policeTp (Player player, int farthestTpDistance);
-        /*
-         * Police teleport
-         * 
-         * @param player The player to teleport
-         * @param farthestTpDistance The farthest distance to teleport a player from another player
-         * 
-         * @return Location The random location given to teleport to
-         */
 
+/*
+ * Police teleport a player
+ * 
+ * @param player The player to teleport
+ * 
+ * @return Location The random location given to teleport to
+ */
 Location policeTp (Player player);
-        /*
-         * Police teleport a player
-         * 
-         * @param player The player to teleport
-         * 
-         * @return Location The random location given to teleport to
-         */
 
+/*
+ * List of all the police (in string uuid format)
+ * 
+ * @return ArrayList<String> List of all the police (in string uuid format)
+ */
 ArrayList<String> listPolice();
-        /*
-         * List of all the police (in string uuid format)
-         * 
-         * @return ArrayList<String> List of all the police (in string uuid format)
-         */
 
 
+/*
+ * Check if a player is in a safe area
+ * 
+ * @param player Player to check
+ * 
+ * @return boolean if the player is in a safe area
+ */
 boolean inSafeArea (Player player);
-        /*
-         * Check if a player is in a safe area
-         * 
-         * @param player Player to check
-         * 
-         * @return boolean if the player is in a safe area
-         */
+
 
 
 }
