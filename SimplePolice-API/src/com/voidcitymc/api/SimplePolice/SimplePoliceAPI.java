aaeb4f6eb8f9b70e52ac1cdd090f00dbee602533
ramdon_person <48343703+ramdon-person@users.noreply.github.com>
2020-07-11 17:40:34 -0700

    Update SimplePoliceAPI.java

--- SimplePolice-API/src/com/voidcitymc/api/SimplePolice/SimplePoliceAPI.java
+++ SimplePolice-API/src/com/voidcitymc/api/SimplePolice/SimplePoliceAPI.java
@@ -34,47 +34,47 @@ boolean isPolice (String uuid);
 
 
 void removePolice(String uuid);
-    /*
-     * Remove a police from the list
-     * 
-     * @param uuid The uuid (in string format) of the player to remove
-     */
+        /*
+         * Remove a police from the list
+         * 
+         * @param uuid The uuid (in string format) of the player to remove
+         */
 
 Location policeTp (Player player, int farthestTpDistance);
-     /*
-      * Police teleport
-      * 
-      * @param player The player to teleport
-      * @param farthestTpDistance The farthest distance to teleport a player from another player
-      * 
-      * @return Location The random location given to teleport to
-      */
+        /*
+         * Police teleport
+         * 
+         * @param player The player to teleport
+         * @param farthestTpDistance The farthest distance to teleport a player from another player
+         * 
+         * @return Location The random location given to teleport to
+         */
 
 Location policeTp (Player player);
-     /*
-      * Police teleport a player
-      * 
-      * @param player The player to teleport
-      * 
-      * @return Location The random location given to teleport to
-      */
+        /*
+         * Police teleport a player
+         * 
+         * @param player The player to teleport
+         * 
+         * @return Location The random location given to teleport to
+         */
 
 ArrayList<String> listPolice();
-     /*
-      * List of all the police (in string uuid format)
-      * 
-      * @return ArrayList<String> List of all the police (in string uuid format)
-      */
+        /*
+         * List of all the police (in string uuid format)
+         * 
+         * @return ArrayList<String> List of all the police (in string uuid format)
+         */
 
 
 boolean inSafeArea (Player player);
-     /*
-      * Check if a player is in a safe area
-      * 
-      * @param player Player to check
-      * 
-      * @return boolean if the player is in a safe area
-      */
+        /*
+         * Check if a player is in a safe area
+         * 
+         * @param player Player to check
+         * 
+         * @return boolean if the player is in a safe area
+         */
 
 
 }
