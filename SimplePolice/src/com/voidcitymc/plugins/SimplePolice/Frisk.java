d8ca223af72694c9c5ef443920fa92bea9125318
ramdon-person <fierceeo@gmail.com>
2020-07-13 15:31:57 -0700

    starting work on frisk system

---
+++ SimplePolice/src/com/voidcitymc/plugins/SimplePolice/Frisk.java
@@ -0,0 +1,15 @@
+package com.voidcitymc.plugins.SimplePolice;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class Frisk implements Listener {
+	
+    @EventHandler
+	public void onFrisk(PlayerInteractEvent event) {
+    	
+	}
+	
+	
+}
