a4fd997f3571d41627389769b659f712408ea65c
ramdon-person <fierceeo@gmail.com>
2020-07-14 10:39:01 -0700

    added messages to frisking and cooldown

--- SimplePolice/src/com/voidcitymc/plugins/SimplePolice/Frisk.java
+++ SimplePolice/src/com/voidcitymc/plugins/SimplePolice/Frisk.java
@@ -2,6 +2,7 @@ package com.voidcitymc.plugins.SimplePolice;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -12,7 +13,7 @@ import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.ChatColor;
 
 public class Frisk implements Listener {
-	
+	private final CooldownManager cooldownManager = new CooldownManager();
     @EventHandler
 	public void onFrisk(PlayerInteractEntityEvent event) {
     	if (event.getRightClicked() instanceof Player) {
@@ -20,46 +21,71 @@ public class Frisk implements Listener {
         	if (work.friskingEnabled() && work.alreadyPolice(event.getPlayer().getUniqueId().toString()) && event.getPlayer().getInventory().getItemInMainHand().getType().equals(work.getFriskStickMaterial())) {
         		//begin frisk
         		Player suspectedPlayer = (Player) event.getRightClicked();
-        		suspectedPlayer.sendMessage(ChatColor.DARK_AQUA+"You are being frisked");
-        		PlayerInventory invToScan = suspectedPlayer.getInventory();
         		
-        		ItemStack[] contents = invToScan.getContents();
-        		int i = 0;
-        		
-        		List<ItemStack> controband = work.getFriskList();
-        		
-        		while (i < controband.size()) {
-        			contents[i].setAmount(1);
-        			i++;
-        		}
-        		i = 0;
-        		
-        		ArrayList<String> textToReturn = new ArrayList<String>();
-        		textToReturn.add(ChatColor.DARK_AQUA+"Controband items found in player");
-        		//Boolean guilty = false;
-        		//Double priceToPay = 0.0;
-        		
-        		while (i < contents.length) {
-        			if (controband.contains(contents[i])) {
-        				if (SPPlugin.getInstance().Controband.getInt("PrecentOfFindingControband")-Math.random()*100 >= 0) {
-            				textToReturn.add(ChatColor.DARK_AQUA+""+invToScan.getContents()[i].getAmount()+"x "+invToScan.getContents()[i].getType().toString());
-            		        //guilty = true;
-            		        event.getPlayer().getInventory().addItem(invToScan.getContents()[i]);
-            		        suspectedPlayer.getInventory().setItem(i,null);
+    			long timeLeft = System.currentTimeMillis() - cooldownManager.getCooldown(event.getPlayer().getUniqueId(), suspectedPlayer.getUniqueId());
+                
+                if (TimeUnit.MILLISECONDS.toSeconds(timeLeft) >= CooldownManager.DEFAULT_COOLDOWN){
+                    cooldownManager.setCooldown(event.getPlayer().getUniqueId(), suspectedPlayer.getUniqueId(), System.currentTimeMillis());
 
 
-        				}
+            		
+            		
+            		
+            		
+            		
+            		
+            		suspectedPlayer.sendMessage(ChatColor.DARK_AQUA+"You are being frisked");
+            		event.getPlayer().sendMessage(ChatColor.DARK_AQUA+"You are now frisking "+suspectedPlayer.getName());
+            		PlayerInventory invToScan = suspectedPlayer.getInventory();
+            		
+            		ItemStack[] contents = invToScan.getContents();
+            		int i = 0;
+            		
+            		List<ItemStack> controband = work.getFriskList();
+            		
+            		while (i < controband.size()) {
+            			contents[i].setAmount(1);
+            			i++;
+            		}
+            		i = 0;
+            		
+            		ArrayList<String> textToReturn = new ArrayList<String>();
+            		Boolean guilty = false;
+            		//Double priceToPay = 0.0;
+            		
+            		while (i < contents.length) {
+            			if (controband.contains(contents[i])) {
+            				if (SPPlugin.getInstance().Controband.getInt("PrecentOfFindingControband")-Math.random()*100 >= 0) {
+                				textToReturn.add(ChatColor.DARK_AQUA+""+invToScan.getContents()[i].getAmount()+"x "+invToScan.getContents()[i].getType().toString());
+                		        guilty = true;
+                		        event.getPlayer().getInventory().addItem(invToScan.getContents()[i]);
+                		        suspectedPlayer.getInventory().setItem(i,null);
 
-        			}
-        			i++;
-        		}
-        		
-                if (textToReturn.size() < 2) {
-                	textToReturn.add(ChatColor.DARK_AQUA+"Nothing");
+
+            				}
+
+            			}
+            			i++;
+            		}
+            		
+            		if (guilty) {
+            			suspectedPlayer.sendMessage(ChatColor.DARK_AQUA+"The police have found illigeal items in your inventory and have taken them, here are the items you have lost:");
+            			suspectedPlayer.sendMessage((String[])textToReturn.toArray());
+            		} else {
+            			suspectedPlayer.sendMessage(ChatColor.DARK_AQUA+"The police have scanned your inventory and have found no illigeal items");
+            		}
+            		
+            		event.getPlayer().sendMessage(ChatColor.DARK_AQUA+"Controband items found in player:");
+                    if (textToReturn.size() < 1) {
+                    	textToReturn.add(ChatColor.DARK_AQUA+"Nothing");
+                    }
+                    
+                    event.getPlayer().sendMessage((String[])textToReturn.toArray());
+            		
+                } else {
+                    event.getPlayer().sendMessage(ChatColor.RED.toString() + "You can frisk this player again in "+ (CooldownManager.DEFAULT_COOLDOWN - TimeUnit.MILLISECONDS.toSeconds(timeLeft)) + " seconds");
                 }
                 
-                event.getPlayer().sendMessage((String[])textToReturn.toArray());
-        		
         	}
         	
     	}
